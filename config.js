1c847c426d6bc70de9220e708e5ea338e61ae813
Ole Melhus <ole@on-it.no>
2019-08-12 09:32:52 +0200

    Move back

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const config = {
 		"logo": "https://www.on-it.no/wp-content/themes/on-it/style/images/on_it_logo.png",
 		"logoLink": "/",
 		"title": "Zpider eShop Docs",
-		"githubUrl": "https://github.com/ON-IT/Zpider-Docs",
+		"githubUrl": "https://github.com/omelhus/zpider-docs",
 		"helpUrl": "",
 		"tweetText": "",
 		"links": [
@@ -17,20 +17,21 @@ const config = {
 	},
 	"sidebar": {
 		"forcedNavOrder": [
+			"/"
 		],
 		"links": [
 			{ "text": "Zpider eShop", "link": "https://zpider.io"},
-			{ "text": "ON IT AS", "link": "https://on-it.no"},
+			{ "text": "ON IT", "link": "https://zpider.io"},
 			{ "text": "Visma", "link": "https://visma.com"}
 		],
 		"frontline": false,
-		"ignoreIndex": true,
+		"ignoreIndex": false,
 	},
 	"siteMetadata": {
 		"title": "Zpider eShop Documentation",
 		"description": "Zpider eShop Documentation",
 		"ogImage": null,
-		"docsLocation": "https://github.com/ON-IT/Zpider-Docs/tree/master/content",
+		"docsLocation": "https://github.com/omelhus/zpider-docs/tree/master/content",
 		"favicon": "https://www.on-it.no/wp-content/themes/on-it/favicon.ico"
 	},
 };
