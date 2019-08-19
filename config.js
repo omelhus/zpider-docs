a381f8476f79847174d817eacd43877a10907844
Ole Melhus <ole@on-it.no>
2019-08-19 14:28:21 +0200

    Add eshop community

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ const config = {
 		],
 		"links": [
 			{ "text": "Zpider eShop", "link": "https://zpider.io"},
-			{ "text": "Zpider Community", "link": "https://spectrum.chat/zpider"},
+			{ "text": "Zpider eShop Community", "link": "https://spectrum.chat/zpider"},
 			{ "text": "ON IT", "link": "https://on-it.no"},
 			{ "text": "Visma", "link": "https://visma.com"}
 		],
